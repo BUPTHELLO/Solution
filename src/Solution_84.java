6d20354f89d59ca7e8752327d515d28aa88dc131
yuxinwei <910599247@qq.com>
2020-05-30 23:03:46 +0800

    add:84

--- src/Solution_84.java
+++ src/Solution_84.java
@@ -1,33 +1,15 @@
 public class Solution_84 {
-
-    public static int largestRectangleArea(int[] heights) {
+    public int largestRectangleArea(int[] heights) {
         int res = 0;
-        for (int i=0;i<heights.length;i++){
-            for (int j = i;j<heights.length;j++){
-//                寻找【i，j】区间的最小值
-                int height = findMin(heights,i,j);
-                res = Math.max(res,height*(j-i+1));
-            }
+        for(int i=0; i<heights.length; i++){
+            int L = i-1;
+            int R = i+1;
+
+            while(L>=0 && heights[L]>=heights[i]) L--;
+            while(R<heights.length && heights[R]>=heights[i]) R++;
+
+            res = Math.max(res,heights[i]*(R-L-1));
         }
         return res;
-
-    }
-
-    /**
-     * 寻找[i,j]区间内的最小值
-     * @param num
-     * @param i
-     * @param j
-     * @return
-     */
-    private static int findMin(int[] num,int i,int j){
-        int res = num[i];
-        for (;i<=j;i++){
-            res = Math.min(res,num[i]);
-        }
-        return res;
-    }
-    public static void main(String[] args) {
-
     }
 }
